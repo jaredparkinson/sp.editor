488c78f686cbcbf2db6af239140d6bddee427dab
Jared Parkinson <jared@parkinson.im>
2018-07-14 14:17:00 -0600

    Init vue project

--- bin/main.js
+++ bin/main.js
@@ -1,1 +1,18 @@
 "use strict";
+// src/idnex.ts
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const vue_1 = __importDefault(require("vue"));
+let v = new vue_1.default({
+    el: "#app",
+    template: `
+    <div>
+        <div>Hello {{name}}!</div>
+        Name: <input v-model="name" type="text">
+    </div>`,
+    data: {
+        name: "World"
+    }
+});
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+// src/idnex.ts
+
+import Vue from "vue";
+
+let v = new Vue({
+    el: "#app",
+    template: `
+    <div>
+        <div>Hello {{name}}!</div>
+        Name: <input v-model="name" type="text">
+    </div>`,
+    data: {
+        name: "World"
+    }
+});

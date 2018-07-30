2f106e509b37f0954dfc27165058b34957d4654c
Jared Parkinson <jared@parkinson.im>
2018-07-30 16:08:05 -0600

    Docker updates

---
+++ docker_upload.ps1
@@ -0,0 +1,13 @@
+docker build . -t scriptures_project_v3
+
+docker save scriptures_project_v3 -o .\scriptures_project_v3
+
+scp scriptures_project_v3 jared@192.168.1.150:/home/jared/v3
+scp scriptures.tar.gz jared@192.168.1.150:/home/jared/v3
+
+
+ssh -t jared@192.168.1.150 'docker load -i scriptures_project_v3'
+
+ssh -t jared@192.168.1.150 'docker kill scriptures_project_v3'
+
+ssh -t jared@192.168.1.150 'docker run -d -e VIRTUAL_HOST=v3.scripturesproject.review -p 10000 -it --rm --name scriptures_project_v3 scriptures_project_v3'
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -3,15 +3,29 @@ import { NavigationService } from '../services/navigation.service';
 import { ChapterService } from '../services/chapter.service';
 import { SaveStateService } from '../services/save-state.service';
 
+import {
+  faBars,
+  faBookOpen,
+  faListUl,
+  faParagraph,
+  faPlus,
+  faGlobe
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
 export class NotesComponent implements OnInit {
+  faBars = faBars;
+  faParagraph = faParagraph;
+  faBookOpen = faBookOpen;
+  faPlus = faPlus;
+  faListUl = faListUl;
+  faGlobe = faGlobe;
   constructor(
     public chapterService: ChapterService,
-    public navService: NavigationService,
+    public navServices: NavigationService,
     public saveState: SaveStateService
   ) {}
 

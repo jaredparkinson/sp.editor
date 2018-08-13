de1f7043974e194157820168f90a77744fc1ab06
Jared Parkinson <jared@parkinson.im>
2018-08-13 05:32:51 -0600

    Changed build_production to remove scripture files before build

--- build_production.ps1
+++ build_production.ps1
@@ -2,3 +2,6 @@ Move-Item .\src\assets\scriptures\ ..
 # npm run electron:linux  
 npm run electron:windows
 Move-Item ..\scriptures .\src\assets\
+
+
+Copy-Item '.\app-builds\sp-editor 3.0.0.exe' ('c:\Users\jared\OneDrive\scripture_file_share\scriptures_project\v3_sp-editor_' + [DateTime]::now.ToString("yyyy-MM-dd_HHmm_fffffff") + '.exe')

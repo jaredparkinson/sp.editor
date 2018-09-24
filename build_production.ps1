49056089c2565c384d245bcdef22c21b806e6d8b
Jared Parkinson <jared@parkinson.im>
2018-09-24 01:18:12 -0600

    script update

--- build_production.ps1
+++ build_production.ps1
@@ -5,4 +5,4 @@ yarn run electron:windows
 Move-Item ..\scriptures .\src\assets\
 
 
-Copy-Item '.\app-builds\sp-editor 3.0.0.exe' ('c:\Users\jared\OneDrive\scripture_file_share\scriptures_project\v3_sp-editor_' + [DateTime]::now.ToString("yyyy-MM-dd_HHmm_fffffff") + '.exe')
+Copy-Item '.\app-builds\sp-edito*.msi' ('c:\Users\jared\OneDrive\scripture_file_share\scriptures_project\v3_sp-editor_' + [DateTime]::now.ToString("yyyy-MM-dd_HHmm_fffffff") + '.exe')
--- docker_upload.ps1
+++ docker_upload.ps1
@@ -3,7 +3,7 @@ docker build . -t scriptures_project_v3
 docker save scriptures_project_v3 -o .\scriptures_project_v3
 
 scp scriptures_project_v3 jared@192.168.1.150:/home/jared/v3
-scp scriptures.tar.gz jared@192.168.1.150:/home/jared/v3
+# scp scriptures.tar.gz jared@192.168.1.150:/home/jared/v3
 
 
 ssh -t jared@192.168.1.150 'docker load -i /home/jared/v3/scriptures_project_v3'

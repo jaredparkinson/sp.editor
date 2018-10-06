29a2bbd1a22d8fb267ad1e131deeaaab2a427e0d
Jared Parkinson <jared@parkinson.im>
2018-10-06 12:27:49 -0600

    moved test data to seperate file

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,7 +1,8 @@
+import * as lodash from 'lodash';
 import { browser, by, element } from 'protractor';
 import { UrlBuilder } from '../src/app/files/UrlBuilder';
 import { AngularElectronPage } from './app.po';
-
+import { UrlData } from './UrlData';
 describe('sp-editor App', () => {
   let page: AngularElectronPage;
 
@@ -21,31 +22,37 @@ describe('sp-editor App', () => {
   it('Natural text should produce the correct urls', () => {
     const urlBuilder = new UrlBuilder();
 
-    expect(urlBuilder.urlParser('1 Nephi 1:1 (1)')).toMatch('1-ne/1.1.1');
-    expect(urlBuilder.urlParser('Abraham 2:9-11, 15-16, 23 (9-24)')).toMatch(
-      'abr/2.9-11,15-16,23.9-24'
-    );
-    expect(urlBuilder.urlParser('Abr 2:9–11, 15–16, 23 (9–24)')).toMatch(
-      'abr/2.9-11,15-16,23.9-24'
-    );
+    const urlData = new UrlData();
+    lodash.each(urlData.urls, url => {
+      expect(urlBuilder.urlParser(url[0])).toMatch(url[1]);
+    });
 
-    expect(urlBuilder.urlParser('Ex. 24:13 ')).toMatch('ex/24.13');
-    expect(urlBuilder.urlParser('Ex 24:13 ')).toMatch('ex/24.13');
-    expect(urlBuilder.urlParser('Exodus 28:1')).toMatch('ex/28.1');
-    expect(urlBuilder.urlParser('Num. 4:12, 47')).toMatch('num/4.12,47');
-    expect(urlBuilder.urlParser('Num 4:12, 47')).toMatch('num/4.12,47');
-    expect(urlBuilder.urlParser('Numbers 4:12, 47')).toMatch('num/4.12,47');
-    expect(urlBuilder.urlParser('Hosea 12:10 (9–11)')).toMatch(
-      'hosea/12.10.9-11'
-    );
-    expect(urlBuilder.urlParser('Hosea 12:10 (9-11)')).toMatch(
-      'hosea/12.10.9-11'
-    );
-    expect(urlBuilder.urlParser('Acts 1:25')).toMatch('acts/1.25');
-    expect(urlBuilder.urlParser('1 Ne. 9:3–4')).toMatch('1-ne/9.3-4');
-    expect(urlBuilder.urlParser('1 Nephi 9:3–4')).toMatch('1-ne/9.3-4');
-    expect(urlBuilder.urlParser('1 Ne. 9:3-4')).toMatch('1-ne/9.3-4');
-    expect(urlBuilder.urlParser('1 Nephi 9:3-4')).toMatch('1-ne/9.3-4');
-    expect(urlBuilder.urlParser('3 Ne 10:19')).toMatch('3-ne/10.19');
+    //   expect(urlBuilder.urlParser('1 Nephi 1:1 (1)')).toMatch('1-ne/1.1.1');
+    //   expect(urlBuilder.urlParser('Abraham 2:9-11, 15-16, 23 (9-24)')).toMatch(
+    //     'abr/2.9-11,15-16,23.9-24'
+    //   );
+    //   expect(urlBuilder.urlParser('Abr 2:9–11, 15–16, 23 (9–24)')).toMatch(
+    //     'abr/2.9-11,15-16,23.9-24'
+    //   );
+
+    //   expect(urlBuilder.urlParser('Ex. 24:13 ')).toMatch('ex/24.13');
+    //   expect(urlBuilder.urlParser('Ex 24:13 ')).toMatch('ex/24.13');
+    //   expect(urlBuilder.urlParser('Exodus 28:1')).toMatch('ex/28.1');
+    //   expect(urlBuilder.urlParser('Num. 4:12, 47')).toMatch('num/4.12,47');
+    //   expect(urlBuilder.urlParser('Num 4:12, 47')).toMatch('num/4.12,47');
+    //   expect(urlBuilder.urlParser('Numbers 4:12, 47')).toMatch('num/4.12,47');
+    //   expect(urlBuilder.urlParser('Hosea 12:10 (9–11)')).toMatch(
+    //     'hosea/12.10.9-11'
+    //   );
+    //   expect(urlBuilder.urlParser('Hosea 12:10 (9-11)')).toMatch(
+    //     'hosea/12.10.9-11'
+    //   );
+    //   expect(urlBuilder.urlParser('Acts 1:25')).toMatch('acts/1.25');
+    //   expect(urlBuilder.urlParser('1 Ne. 9:3–4')).toMatch('1-ne/9.3-4');
+    //   expect(urlBuilder.urlParser('1 Nephi 9:3–4')).toMatch('1-ne/9.3-4');
+    //   expect(urlBuilder.urlParser('1 Ne. 9:3-4')).toMatch('1-ne/9.3-4');
+    //   expect(urlBuilder.urlParser('1 Nephi 9:3-4')).toMatch('1-ne/9.3-4');
+    //   expect(urlBuilder.urlParser('3 Ne 10:19')).toMatch('3-ne/10.19');
+    // });
   });
 });

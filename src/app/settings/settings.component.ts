8e193e6e6cdc87282dc21c158ea1204ed83a1fb7
Jared Parkinson <jared@parkinson.im>
2018-10-19 09:51:14 -0600

    Working on zip files

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,7 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { SaveStateService } from '../services/save-state.service';
-import { NavigationService } from '../services/navigation.service';
+import * as jszip from 'jszip';
+import * as _ from 'lodash';
+import * as pako from 'pako';
 
+import { NavigationService } from '../services/navigation.service';
+import { SaveStateService } from '../services/save-state.service';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
@@ -10,8 +14,30 @@ import { NavigationService } from '../services/navigation.service';
 export class SettingsComponent implements OnInit {
   constructor(
     public saveState: SaveStateService,
-    public navServices: NavigationService
+    public navServices: NavigationService,
+    private httpClient: HttpClient
   ) {}
 
   ngOnInit() {}
+
+  download(file: string) {
+    this.httpClient
+      .get('assets/scriptures/' + file + '.zip', {
+        observe: 'body',
+        responseType: 'arraybuffer'
+      })
+      .subscribe(async data => {
+        // const i = pako.gzip(, {});
+        const zip = await jszip.loadAsync(data);
+        zip.forEach(async file2 => {
+          const contents = await zip.file(file2).async('text');
+          localStorage.setItem(file2, contents);
+        });
+        // console.log(zip);
+
+        console.log('Finished');
+
+        // pako.inflate(data);
+      });
+  }
 }

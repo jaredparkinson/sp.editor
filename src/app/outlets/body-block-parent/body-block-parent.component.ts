8aac65c26932a550f8478847323b49707e3430c0
Jared Parkinson <jared@parkinson.im>
2019-03-11 08:10:42 -0600

    More navigation improvements

--- src/app/outlets/body-block-parent/body-block-parent.component.ts
+++ src/app/outlets/body-block-parent/body-block-parent.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { SaveStateService } from '../../services/save-state.service';
 
 @Component({
   selector: 'app-body-block-parent',
   templateUrl: './body-block-parent.component.html',
-  styleUrls: ['./body-block-parent.component.scss']
+  styleUrls: ['./body-block-parent.component.scss'],
 })
 export class BodyBlockParentComponent implements OnInit {
+  constructor(public saveState: SaveStateService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

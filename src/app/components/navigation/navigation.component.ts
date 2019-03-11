664b2abcf5c4a72d1fd45667e51bf45bd01de407
Jared Parkinson <jared@parkinson.im>
2019-03-11 06:31:02 -0600

    Navigation now works when you press it anywhere

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Navigation } from '../../modelsJson/Chapter';
 import { NavigationService } from '../../services/navigation.service';
 
@@ -10,7 +11,17 @@ import { NavigationService } from '../../services/navigation.service';
 export class NavigationComponent implements OnInit {
   @Input() public navigation: Navigation;
   @Input() public navigations: Navigation[];
-  constructor(public navigationService: NavigationService) {}
+  constructor(
+    public navigationService: NavigationService,
+    public router: Router,
+  ) {}
 
   ngOnInit() {}
+
+  gotoChapter() {
+    this.navigation.visible = this.navigation.visible
+      ? !this.navigation.visible
+      : true;
+    this.router.navigateByUrl(`${this.navigation.url}`);
+  }
 }

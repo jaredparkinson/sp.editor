98de7e8868fb4882dffb2badf3e1249076ef3851
Jared Parkinson <jared@parkinson.im>
2019-04-19 02:16:58 -0600

    Aligned highlight colors

--- src/app/components/wtag-popup/wtag-popup.component.ts
+++ src/app/components/wtag-popup/wtag-popup.component.ts
@@ -7,7 +7,18 @@ import { WTagService } from '../../services/wtag-builder.service';
   styleUrls: ['./wtag-popup.component.scss'],
 })
 export class WTagPopupComponent implements OnInit {
-  public colors: string[] = ['yellow','lightblue','red', 'blue', 'green'];
+  public colors: string[] = [
+    '#fff8aa',
+    '#aaf6ff',
+    '#d6ffaa',
+    '#FF9B9F',
+    '#D7D5FC',
+    '#FFBF98',
+    '#ffc2e9',
+    '#DDDDDD',
+    '#DFCDBA',
+    '#A8C7E6',
+  ];
   public showColorPalette = false;
   constructor(public wTagService: WTagService) {}
   public highlightClick() {
